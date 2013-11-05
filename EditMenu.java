ce129a0e10ac52e1c6c9a598b20dc9a8d9bfa28b
agn0605 <aney@mail.umw.edu>
2013-11-05 16:38:12 -0500

    Update EditMenu.java

--- EditMenu.java
+++ EditMenu.java
@@ -1,7 +1,8 @@
 
-//All these extra imports are to make the cut/copy/paste buttons function
-//Also (hopefully) covers undo/redo
+//All these extra imports are to make the pre-defined
+//buttons for cut/copy/paste/undo/redo function
 import java.io.*;
+import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.datatransfer.*;
@@ -12,11 +13,16 @@ import javax.swing.event.*;
 import javax.swing.undo.*;
 
 public class EditMenu {  
-        
-  // adds the file menu to the menu bar        
-  public static JMenuBar addEditMenu(JMenuBar menubar) {
+	
+	//private AbstractDocument doc;
+	private UndoManager undo = new UndoManager();
+	private UndoAction undoAction;
+	private RedoAction redoAction;
+	
+  // adds the edit menu to the menu bar        
+  public JMenuBar addEditMenu(JMenuBar menubar) {
 
-    // add the file menu
+    // add the edit menu
     JMenuItem menuItem = null;
     JMenu edit = new JMenu("Edit");
     edit.setMnemonic(KeyEvent.VK_E);
@@ -36,11 +42,84 @@ public class EditMenu {
     menuItem.setMnemonic(KeyEvent.VK_P);
     edit.add(menuItem);
     
-    // add file to the menubar
+    edit.addSeparator();
+    
+    undoAction = new UndoAction();
+    edit.add(undoAction);
+
+    redoAction = new RedoAction();
+    edit.add(redoAction);
+    
+	//doc.addUndoableEditListener(new MyUndoableEditListener());
+	
+	// add file to the menubar
     menubar.add(edit);
     
     return menubar;
+  }
+    
+    class UndoAction extends AbstractAction {
+        public UndoAction() {
+            super("Undo");
+            setEnabled(false);
+        }
+ 
+        public void actionPerformed(ActionEvent e) {
+            try {
+                undo.undo();
+            } catch (CannotUndoException ex) {
+                System.out.println("Unable to undo: " + ex);
+                ex.printStackTrace();
+            }
+            updateUndoState();
+            redoAction.updateRedoState();
+        }
+ 
+        protected void updateUndoState() {
+            if (undo.canUndo()) {
+                setEnabled(true);
+                putValue(Action.NAME, undo.getUndoPresentationName());
+            } else {
+                setEnabled(false);
+                putValue(Action.NAME, "Undo");
+            }
+        }
+    }
+    
+  class RedoAction extends AbstractAction {
+        public RedoAction() {
+            super("Redo");
+            setEnabled(false);
+        }
+ 
+        public void actionPerformed(ActionEvent e) {
+            try {
+                undo.redo();
+            } catch (CannotRedoException ex) {
+                System.out.println("Unable to redo: " + ex);
+                ex.printStackTrace();
+            }
+            updateRedoState();
+            undoAction.updateUndoState();
+        }
+ 
+        protected void updateRedoState() {
+            if (undo.canRedo()) {
+                setEnabled(true);
+                putValue(Action.NAME, undo.getRedoPresentationName());
+            } else {
+                setEnabled(false);
+                putValue(Action.NAME, "Redo");
+            }
+        }
+    }
 
-   
+  protected class MyUndoableEditListener implements UndoableEditListener {
+	  public void undoableEditHappened(UndoableEditEvent e) {
+		//Remember the edit and update the menus
+		undo.addEdit(e.getEdit());
+		undoAction.updateUndoState();
+		redoAction.updateRedoState();
+	  }
   }
 }
